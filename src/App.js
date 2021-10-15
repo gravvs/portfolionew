fda174918d412f4a44a325775dcabbac0b087b13
gravvs <62290617+gravvs@users.noreply.github.com>
2021-10-15 12:07:37 +0200

    add body background, add heder content , add header styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import './styles/themes/default/theme.scss'
 import Header from "./components/Header";
+
 
 function App() {
   return (
-    <div>
-      Hello
+    <div className="app">
       <Header />
+      
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,41 @@
-import React from "react";
+import React,{ useState } from "react";
 
 export default function Header() {
+  const [active, setActive] = useState(true)
+  
+  const handleActive = () => {
+    setActive(!active)
+  }
   return (
-    <div>
-      Header
+    <div className="header">
+      <div className="header__sign">
+        <h2>&#2417;B.C</h2>
+        <h4>Bartosz Czenczek Portfolio</h4>
+        <h4>Front End Developer</h4>
+      </div>
+      <div className="header__menu">
+        <h4 onClick={handleActive}>Menu &#x022BD;</h4>
+        {active && <div>
+          <p>My portfolio</p>
+          <p>Service</p>
+          <p>Awards</p>
+          <p>Technology</p>
+          <p>Contact Me</p>
+        </div>}
+      </div>
+      <div className="header__square">
+        <p>Engage</p>
+        <p>Design</p>
+        <p>Evolve</p>
+      </div>
+      <div className="header__header">
+        <h2>
+          My ideology is to engage my best skills and technology <br /> in the
+          process of design
+          <br />
+          <span>to make your ideas evolve</span>
+        </h2>
+      </div>
     </div>
   );
 }
