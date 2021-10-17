d30ee461e942f6f1b7ceda62ac86593e13fdf10e
gravvs <62290617+gravvs@users.noreply.github.com>
2021-10-17 19:48:24 +0200

    add project styles, add project content

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,50 +6,52 @@ import booking from "../picture/booking.png";
 
 export default function Projects() {
   return (
-    <div>
+    <div className="project">
       <h1>My projects</h1>
-      <div>
+      <div className="project__header">
         <p>See all projects</p>
-        <img src={play} alt="arrow"/>
+        <img src={play} alt="arrow" />
       </div>
-      <div>
-        <div>
+      <div className="project__wrapper">
+        <div className="project__subwrapper-first">
           <div className="project__first">
-            <img src={first} alt="portfolio"/>
+            <img src={first} alt="portfolio" />
             <div>
               <p>First portfolio</p>
               <p>|Website</p>
             </div>
           </div>
-          <div>
+          <div className="project__button">
             <p>See more</p>
-            <img src={play} alt="arrow"/>
+            <img src={play} alt="arrow" />
           </div>
         </div>
-        <div>
-          <div className="project__first">
-            <img src={quiz} alt="quiz"/>
-            <div>
-              <p>Quiz with categories</p>
-              <p>|Quiz app</p>
+        <div className="project__subwrapper-second">
+          <div className="project__second-wrapper">
+            <div className="project__second">
+              <img src={quiz} alt="quiz" />
+              <div>
+                <p>Quiz with categories</p>
+                <p>|Quiz app</p>
+              </div>
+            </div>
+            <div className="project__button">
+              <p>See more</p>
+              <img src={play} alt="arrow" />
             </div>
           </div>
-          <div>
-            <p>See more</p>
-            <img src={play} alt="arrow"/>
-          </div>
-        </div>
-        <div>
-          <div className="project__first">
-            <img src={booking} alt="booking"/>
-            <div>
-              <p>Clone of booking</p>
-              <p>|Static website</p>
+          <div className="project__second-wrapper">
+            <div className="project__third">
+              <img src={booking} alt="booking" />
+              <div>
+                <p>Clone of booking</p>
+                <p>|Static website</p>
+              </div>
             </div>
-          </div>
-          <div>
-            <p>See more</p>
-            <img src={play} alt="arrow"/>
+            <div className="project__button">
+              <p>See more</p>
+              <img src={play} alt="arrow" />
+            </div>
           </div>
         </div>
       </div>
