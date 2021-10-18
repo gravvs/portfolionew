a43675a6abad97b0151ebeed6b53ca7c9ea75b56
gravvs <62290617+gravvs@users.noreply.github.com>
2021-10-18 13:41:51 +0200

    add footer component, add footer content , add img to footer component , add styles

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import Main from "./components/Main";
 import Projects from './components/Projects';
 import Technology from './components/Technology';
+import Footer from './components/Footer';
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Main />
       <Projects />
       <Technology />
+      <Footer />
     </div>
   );
 }
