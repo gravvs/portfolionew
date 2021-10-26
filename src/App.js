d76583dcfe2cd14ec3ddf4d22d8bc27ffba69b14
gravvs <62290617+gravvs@users.noreply.github.com>
2021-10-26 20:12:00 +0200

    add images, add hamburger menu

--- src/App.js
+++ src/App.js
@@ -6,16 +6,20 @@ import Technology from './components/Technology';
 import Footer from './components/Footer';
 import Client from './components/Client';
 import Categories from './components/Categories';
-import { useRef } from 'react';
+import Hamburger from './components/Hamburger'
+import { useRef, useState } from 'react';
 
 function App() {
+  const [show, setShow] = useState(false)
   const refMain = useRef(null);
   const refCategories = useRef(null);
   const refTech = useRef(null);
   const refContact = useRef(null);
+  
   return (
     <div className="app">
-      <Header {...{refMain,refCategories,refTech,refContact}}/>
+      <Header {...{refMain,refCategories,refTech,refContact,show,setShow}}/>
+      <Hamburger show={show}/>
       <Main refMain={refMain}/>
       <Categories refCategories={refCategories}/>
       <Projects refMain={refMain}/>
--- src/components/Hamburger.js
+++ src/components/Hamburger.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
+import menu from '../picture/menu.png'
+import contact from '../picture/contact-mail.png'
+import cpu from '../picture/cpu.png'
+import portfolio from '../picture/portfolio.png'
+import tech from '../picture/technical-support.png'
 
-export default function Hamburger() {
+export default function Hamburger(props) {
+    const [active, setActive] = useState(false)
+    const show = active ? " active" : "";
     return (
-        <div>
+        <>
+        {props.show && 
+        <div className={`hamburger${show}`} >
+            <div className="hamburger__menu" onClick={()=>setActive(prevActive=>!prevActive)}><img src={menu} alt="menu"/></div>
             
+            <div className="hamburger__subwrapper">
+                <div className="hamburger__items"><img src={portfolio} alt="portfolio"/></div>
+                <div className="hamburger__items"><img src={tech} alt="categories"/></div>
+                <div className="hamburger__items"><img src={cpu} alt="technology"/></div>
+                <div className="hamburger__items"><img src={contact} alt="contact me"/></div>
+            </div>
         </div>
+}
+        </>
     )
 }
